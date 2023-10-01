4cd3b25b291bc09b062393d321fad1d30df1fb86
shino <shino.hinaduki@gmail.com>
2023-10-01 15:18:16 +0900

    refactor: `parsePackage`関数に整理、argpaseを使用

--- extractunitypackage.py
+++ extractunitypackage.py
@@ -4,103 +4,121 @@
 #
 # Extracts files/folders from a .unitypackage file, reading the 'pathname'
 # files it contains to rebuild a "readable" file/folder structure.
-#
-# Usage: extractunitypackage.py input_file [output_path]
-#
-# "input_file" should be a .unitypackage file. The part of the filename
-# before the extension will be used as the name of the directory that the 
-# packages contents will be extracted to.
-#
-# "output_path" is an optional path where the package's files will be
-# extracted to. If omitted, the current working directory is used. If
-# specified, the path should already exist.
-
-# Update (by Entwicklerpages): Simple fixes for python3 support. Now works on both versions.
 
 import os
 import stat
 import shutil
 import sys
 import tarfile
+import argparse
 
-if len(sys.argv) < 2:
-	print ('No input file specified.')
-	sys.exit()
 
-name, extension = os.path.splitext(sys.argv[1])
+def parsePackage(srcPath: str, outputBaseDir: str = None) -> (bool, str):
+    """Extract UnityPackage"""
+    name, extension = os.path.splitext(srcPath)
 
-outputDir = ''
-if len(sys.argv) > 2:
-	outputDir = os.path.join(sys.argv[2], name)
-else:
-	outputDir = './' + name
-workingDir = './.working'
+    outputDir = ""
+    if outputBaseDir is None:
+        outputDir = "./" + name
+    else:
+        outputDir = os.path.join(outputBaseDir, name)
+    workingDir = "./.working"
 
-# can't proceed if the output dir exists already
-# but if the temp working dir exists, we clean it out before extracting
-if os.path.exists(outputDir):
-	print ('Output dir "' + outputDir + '" exists. Aborting.')
-	sys.exit();
-if os.path.exists(workingDir):
-	shutil.rmtree(workingDir)
+    # can't proceed if the output dir exists already
+    # but if the temp working dir exists, we clean it out before extracting
+    if os.path.exists(outputDir):
+        return (False, f"Output dir {outputDir} exists. Aborting.")
 
-# extract .unitypackage contents to a temporary space
-tar = tarfile.open(sys.argv[1], 'r:gz')
-tar.extractall(workingDir);
-tar.close()
+    if os.path.exists(workingDir):
+        shutil.rmtree(workingDir)
 
-# build association between the unitypackage's root directory names
-# (which each have 1 asset in them) to the actual filename (stored in the 'pathname' file)
-mapping = {}
-for i in os.listdir(workingDir):
-	rootFile = os.path.join(workingDir, i)
-	asset = i
+    # extract .unitypackage contents to a temporary space
+    tar = tarfile.open(srcPath, "r:gz")
+    tar.extractall(workingDir)
+    tar.close()
 
-	if os.path.isdir(rootFile):
-		realPath = ''
+    # build association between the unitypackage's root directory names
+    # (which each have 1 asset in them) to the actual filename (stored in the 'pathname' file)
+    mapping = {}
+    for i in os.listdir(workingDir):
+        rootFile = os.path.join(workingDir, i)
+        asset = i
 
-		# we need to check if an 'asset' file exists (sometimes it won't be there
-		# such as when the 'pathname' file is just specifying a directory)
-		hasAsset = False
+        if os.path.isdir(rootFile):
+            realPath = ""
 
-		for j in os.listdir(rootFile):
-			# grab the real path
-			if j == 'pathname':
-				lines = [line.strip() for line in open(os.path.join(rootFile, j))]
-				realPath = lines[0]     # should always be on the first line
-			elif j == 'asset':
-				hasAsset = True
+            # we need to check if an 'asset' file exists (sometimes it won't be there
+            # such as when the 'pathname' file is just specifying a directory)
+            hasAsset = False
 
-		# if an 'asset' file exists in this directory, then this directory
-		# contains a file that should be moved+renamed. otherwise we can
-		# ignore this directory altogether...
-		if hasAsset:
-			mapping[asset] = realPath
+            for j in os.listdir(rootFile):
+                # grab the real path
+                if j == "pathname":
+                    lines = [line.strip() for line in open(os.path.join(rootFile, j))]
+                    realPath = lines[0]  # should always be on the first line
+                elif j == "asset":
+                    hasAsset = True
 
-# mapping from unitypackage internal filenames to real filenames is now built
-# walk through them all and move the 'asset' files out and rename, building
-# the directory structure listed in the real filenames we found as we go
+            # if an 'asset' file exists in this directory, then this directory
+            # contains a file that should be moved+renamed. otherwise we can
+            # ignore this directory altogether...
+            if hasAsset:
+                mapping[asset] = realPath
 
-os.makedirs(outputDir)
+    # mapping from unitypackage internal filenames to real filenames is now built
+    # walk through them all and move the 'asset' files out and rename, building
+    # the directory structure listed in the real filenames we found as we go
 
-for asset in mapping:
-	path, filename = os.path.split(mapping[asset])
+    os.makedirs(outputDir)
 
-	destDir = os.path.join(outputDir, path)
-	destFile = os.path.join(destDir, filename)
-	source = os.path.join(workingDir, asset, 'asset');
+    for asset in mapping:
+        path, filename = os.path.split(mapping[asset])
 
-	if not os.path.exists(destDir):
-		os.makedirs(destDir)
+        destDir = os.path.join(outputDir, path)
+        destFile = os.path.join(destDir, filename)
+        source = os.path.join(workingDir, asset, "asset")
 
-	shutil.move(source, destFile)
-	
-	# change file permissions for unix because under mac os x 
-	# (perhaps also other unix systems) all files are marked as executable
-	# for safety reasons os x prevent the access to the extracted files
-	os.chmod(destFile, stat.S_IRUSR | stat.S_IWUSR | stat.S_IRGRP | stat.S_IROTH)
-	
-	print (asset + ' => ' + mapping[asset])
+        if not os.path.exists(destDir):
+            os.makedirs(destDir)
 
-# done, cleanup any leftovers...
-shutil.rmtree(workingDir)
+        shutil.move(source, destFile)
+
+        # change file permissions for unix because under mac os x
+        # (perhaps also other unix systems) all files are marked as executable
+        # for safety reasons os x prevent the access to the extracted files
+        os.chmod(destFile, stat.S_IRUSR | stat.S_IWUSR | stat.S_IRGRP | stat.S_IROTH)
+
+        print(asset + " => " + mapping[asset])
+
+    # done, cleanup any leftovers...
+    shutil.rmtree(workingDir)
+
+    return (True, outputDir)
+
+
+parser = argparse.ArgumentParser(description="UnityPackage Extractor")
+parser.add_argument(
+    "-i",
+    "--input_file",
+    type=str,
+    help=".unitypackage file. The part of the filename before the extension will be used as the name of the directory that the packages contents will be extracted to.",
+)
+parser.add_argument(
+    "-o",
+    "--output_dir",
+    type=str,
+    help="optional path where the package's files will be extracted to. (If omitted, the current working directory is used)",
+)
+args = parser.parse_args()
+print(args)
+if args.input_file is not None:
+    # CUI
+    (successful, msg) = parsePackage(
+        srcPath=args.input_file, outputBaseDir=args.output_dir
+    )
+    if not successful:
+        print(msg)
+        sys.exit(1)
+    else:
+        print("")
+        print(f"Done. Result saved in {msg}.")
